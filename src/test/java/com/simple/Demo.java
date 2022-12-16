24f1a634139f027fa9c9ea2c4d0e2f201a8b8403
pc <pc@support.wondershare.net>
2022-12-16 11:07:21 +0530

    Update the code

--- src/test/java/com/simple/Demo.java
+++ src/test/java/com/simple/Demo.java
@@ -5,6 +5,9 @@ public class Demo {
 	public static void main(String[] args) {
 
 		System.out.println("First program create a local repository");
+		
+		
+		System.out.println("clone addto index commit push pull");
 	}
 
 }
