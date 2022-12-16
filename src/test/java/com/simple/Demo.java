70d7d30274950d79fd9060dbb0b50e0983a6e506
pc <pc@support.wondershare.net>
2022-12-16 10:32:53 +0530

    created EasyBuy project and added initial files

--- src/test/java/com/simple/Demo.java
+++ src/test/java/com/simple/Demo.java
@@ -3,8 +3,8 @@ package com.simple;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("First program create a local repository");
 	}
 
 }
